cce24b840eb86941ff19eed0f01bc3d5d03c38c4
Mehebul02 <mehebul2123@gmail.com>
2025-03-15 23:52:18 +0600

    Added the footer

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,5 +1,6 @@
 
 import logo from './images/Logo.png'
 import download_app from './images/Download App_.png'
+import payment_methods from './images/Payment Methods.png'
 
-export {logo,download_app}
+export {logo,download_app,payment_methods}
